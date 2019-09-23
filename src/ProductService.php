53e24fb171f6148d60a5ae8f343432aac8f4f792
Mehran Hassanzadeh <hassanzadeh.me@gmail.com>
2019-09-23 17:02:30 +0330

    refact

--- src/ProductService.php
+++ src/ProductService.php
@@ -8,7 +8,6 @@
  * $baseInfo BaseInfo
  */
 namespace Pod\Product\Service;
-require __DIR__ . '/../vendor/autoload.php';
 
 use Pod\Base\Service\BaseService;
 use Pod\Base\Service\ApiRequestHandler;
